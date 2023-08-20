0833691eea5a802230be41274affb9c23fd9a52f
WinsonHuang <hwangwc@qq.com>
2023-08-20 10:20:15 +0800

    Add controller method for user feature

--- src/main/java/demomybatis/api/DemomybatisController.java
+++ src/main/java/demomybatis/api/DemomybatisController.java
@@ -1,9 +1,18 @@
 package demomybatis.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import demomybatis.domain.DemoUser;
+import demomybatis.mapper.DemoUserMapper;
 
 @RestController
 @RequestMapping(path = "/api/user")
@@ -13,6 +22,38 @@ public class DemomybatisController {
     @GetMapping(path = "/test")
     public String test() {
         return "Controller Available.\n";
+    }
+
+
+    @Autowired
+    DemoUserMapper userMapper;
+
+    @GetMapping
+    public List<DemoUser> getAllUser() {
+        return userMapper.selectAllUser();
+    }
+
+    @PostMapping(consumes = "application/json")
+    public DemoUser addUser(@RequestBody DemoUser user) {
+        userMapper.insertUser(user);
+        return user;
+    }
+
+    @PostMapping(path = "/update", consumes = "application/json")
+    public DemoUser updateUser(@RequestBody DemoUser user) {
+        userMapper.updateUserById(user);
+        return userMapper.selectUserById(user.getId());
+    }
+
+    @GetMapping(path = "/{id}")
+    public DemoUser getUserById(@PathVariable("id") Integer id) {
+        return userMapper.selectUserById(id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String deleteUserById(@PathVariable("id") Integer id) {
+        userMapper.deleteUserById(id);
+        return "User id: " + id + " deleted.\n";
     }
     
 }
