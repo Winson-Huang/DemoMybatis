2c9097ca15292a23e2e0b2d753ce590a077f7a21
WinsonHuang <hwangwc@qq.com>
2023-08-20 08:42:09 +0800

    Create project

---
+++ src/main/java/demomybatis/DemomybatisApplication.java
@@ -0,0 +1,13 @@
+package demomybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomybatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demomybatis/DemomybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package demomybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
